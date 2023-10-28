a74d0c31a73cd200a0b7ab37dfd86fbbcdb52f7d
SonicBOOM <104025796+subhamsaini1@users.noreply.github.com>
2023-10-28 13:59:04 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,8 +1,13 @@
-resource "local_file" "things-to-do" {
-  filename     = "~/things-to-do.txt"
-  content  = "Clean my room before Christmas\nComplete the CKA Certification!"
+provider "aws" {
+  region = "ap-south-1"
 }
-resource "local_file" "more-things-to-do" {
-  filename     = "~/more-things-to-do.txt"
-  content  = "Learn how to play Astronomia on the guitar!"
+resource "aws_s3_bucket" "example" {
+  bucket = "test-s3-bucket-67859991"
+  acl = "private"
+  versioning {
+    enabled = true
+  }
+tags = {
+    Environment = "test"
+  }
 }
