f4834bc8e947ccb4367a6998a3aad96faaadda9f
SonicBOOM <104025796+subhamsaini1@users.noreply.github.com>
2023-10-29 16:58:00 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,11 @@
 module "ec2_instance" {
-  source  = "terraform-aws-modules/ec2-instance/aws"
-  version = "5.2.1"
-  name = "single-instance"
-  instance_type = "t2.micro"
+        source = "terraform-aws-modules/ec2-instance/aws"
+        name = "single-instance"
+        instance_type = "t2.micro"
+        vpc_security_group_ids = ["sg-0e686f89153c567ba"]
+        subnet_id = "subnet-05333f6c955722f43"
+        tags = {
+                Terraform   = "true"
+                Environment = "dev"
+        }
 }
